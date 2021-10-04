152de1fa895c327ecdd5d7b052aeb115c301de9a
Xayal V. Isaxanov <IsaxanovXV@kapitalho.az>
2021-10-04 04:00:04 +0400

    frontend added

---
+++ frontend/changelog.md
@@ -0,0 +1,11 @@
+# Added:
+
+tsconfig rules : noUnusedLocals noUnusedParameters
+"include": ["src", "src/**/*.ts", ]
+
+# TODO:
+
+Add new start script
+Add json mock api
+Register service-workers
+Add redux-saga
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from "components/CounterExample";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
